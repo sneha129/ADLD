1e01d6bb818e64ff0fbd3d864a3c2d8fd24d144f
sneha129 <83950860+sneha129@users.noreply.github.com>
2023-05-22 14:12:07 +0530

    Add files via upload

---
+++ or_gate/or_gate_tb.v
@@ -0,0 +1,25 @@
+module orgate_tb();
+reg a,b;
+wire c;
+orgate DUT(.a(a),.b(b),.c(c));
+initial begin 
+$monitor("a=%b b=%b c=%b",a,b,c);
+a=0;
+b=0;
+#10;
+a=0;
+b=1;
+#10;
+a=1;
+b=0;
+#10;
+a=1;
+b=1;
+#10 $finish;
+end 
+endmodule
+module orgate(a,b,c);
+input a,b;
+output c;
+assign c= a | b;
+endmodule
